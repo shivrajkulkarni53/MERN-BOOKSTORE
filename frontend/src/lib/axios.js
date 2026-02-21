0c5aee9fa5fdfa9d6c49eeab463f0f1df4a4dafb
shivrajkulkarni53 <chmcs2026.shivraj.kulkarni@gmail.com>
2026-02-21 12:33:06 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL: 'http://localhost:3000'
+    //baseURL: 'http://localhost:3000'
+    baseURL: 'http://mern-bookstore-2lag.onrender.com'
 })
 export default api
